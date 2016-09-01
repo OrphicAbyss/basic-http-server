85cb98f7e5df78cdb7008fdc01d5e13010b14e7b
Chris Hallson <cm.hallson@gmail.com>
2016-09-01 17:24:34 +0100

    Allow for creating https servers.

--- index.js
+++ index.js
@@ -5,15 +5,27 @@ var http = require("http"),
 
 class BasicHTTPServer {
     constructor (options){
+        this.useHttp = options.useHttp !== undefined && options.useHttp !== null ? options.useHttp : true;
+        this.useHttps = options.useHttps !== undefined && options.useHttps !== null ? options.useHttps : false;
         this.port = options.port || "8080";
-        this.server = null;
+        this.sslPort = options.sslPort || "8081";
+        this.httpOptions = options.httpOptions || {};
+        this.httpsOptions = options.httpsOptions || {};
+        this.serverHttp = null;
+        this.serverHttps = null;
         this.routes = [];
         this.defaultRoute = null;
     }
 
     start () {
-        this.server = http.createServer(this.handleRequest.bind(this));
-        this.server.listen(this.port);
+        if (this.useHttp) {
+            this.serverHttp = http.createServer(this.httpOptions, this.handleRequest.bind(this));
+            this.serverHttp.listen(this.port);
+        }
+        if (this.useHttps) {
+            this.serverHttps = https.createServer(this.httpsOptions, this.handleRequest.bind(this));
+            this.serverHttps.listen(this.port);
+        }
     }
 
     registerDefaultHandler (handler) {
@@ -47,13 +59,44 @@ class BasicHTTPServer {
         }
     }
     
-    getRawServer () {
-        return this.server;
+    getRawHttpServer () {
+        return this.serverHttp;
+    }
+
+    getRawHttpsServer () {
+        return this.serverHttps;
     }
 
     stop (callback) {
-        this.server.close((err) => {
-            this.server = null;
+        if (this.serverHttp) {
+            this.stopHttp((err) => {
+                if (this.serverHttps) {
+                    this.stopHttps((err2) => {
+                        callback(err || err2);
+                    });
+                } else {
+                    callback(err, null);
+                }
+            });
+        } else if (this.serverHttps) {
+            this.stopHttps(callback);
+        } else {
+            callback();
+        }
+    }
+
+    stopHttp (callback) {
+        this.serverHttp.close((err) => {
+            this.serverHttp = null;
+            if (callback) {
+                callback(err, null);
+            }
+        });
+    }
+
+    stopHttps (callback) {
+        this.serverHttps.close((err) => {
+            this.serverHttps = null;
             if (callback) {
                 callback(err, null);
             }
