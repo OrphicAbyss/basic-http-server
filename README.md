0720dfd4e3b33d18cdea70dc3d84f74526e783f0
Chris (DrLabman) Hallson <drlabman@gluonporridge.net>
2016-09-11 11:09:37 +0100

    More details in readme and adjust api and project name before publishing.

--- README.md
+++ README.md
@@ -1,21 +1,41 @@
-# basic-http-server
-Basic HTTP server library for Node.js focused on creating simple REST services or Web-apps with minimal dependencies.
+# microservice-base
+Microservice base library for Node.js to handle most boiler plate of setting up a microservice.
 
 ## Project Objectives
-* Simplify the creating of a HTTP server for static files and web services
-* Have minimal project dependencies
+* Base for building a microservice to reduce the boiler plate code in other codebases
+* Minimal project dependencies for reducing the chance of a broken build from a broken dependency
+* Support for both http and https
 
 ## Classes
-* __BasicHTTPServer__ - Main server class
-* __BaseRoute__ - Base class for routes, expects extending class to implement `handleRequest (request, response)`
-* __NullRoute__ - Route that always returns 404. This can be used as the default handler when only sub-routes are required.
-* __JSONRoute__ - Simple route which handles JSON parsing of the message body object. Expects extending classes to implement `respond (request, response, data)`
+* __MicroService__ - Main server class
+* __RouteBase__ - Base class for routes, expects extending class to implement `handleRequest (request, response)`
+* __RouteNull__ - Route that always returns 404. This can be used as the default handler when only sub-routes are required.
+* __RouteJSON__ - Simple route which handles JSON parsing of the message body object. Expects extending classes to implement `respond (request, response, data)`
+* __StaticFileServer__ - Static file server can be used to serve files from disk
+
+## Usage
+The most basic setup has one route which is setup as the default route. The default route handles all
+requests which don't match any other route.
+1) Create the service
+1) Create a route
+1) Register the route as the default route
+1) Start the server
+
+## Service Options
+* __useHttp__ (Default: true) When true a http server is started
+* __useHttps__ (Default: false) When true a https server is started
+* __port__  (Default: 8080) Set the listening port of the http server (if enabled)
+* __sslPort__  (Default: 8081) Set the listening port of the https server (if enabled)
+* __httpsOptions__ (Default: {}) Options to pass to the https server (to pass in the key and cert of your server)
 
 ## Usage Example
 
-    const BasicHTTPServer = require("basic-http-server");
+_Create a simple file server:_
 
-    const fileHandler = new BasicHTTPServer.StaticFileServer("/", "./", "index.html");
-    const httpServer = new BasicHTTPServer({port: 8080});
-    httpServer.registerDefaultHandler(fileHandler);
-    httpServer.start();
+    const microBase = require("microservice-base");
+
+    const fileHandler = new microBase.StaticFileServer("/", "./", "index.html");
+    const microserver = new microBase.MicroService({port: 8080});
+    microserver.registerDefaultHandler(fileHandler);
+    microserver.start();
+
--- index.js
+++ index.js
@@ -1,125 +1,13 @@
 "use strict";
 
-var http = require("http"),
-    https = require("https");
+const Server = require("./lib/server");
+const Routes = require("./lib/route");
+const StaticFileServer = require("./lib/static");
 
-class BasicHTTPServer {
-    constructor (options){
-        this.useHttp = options.useHttp !== undefined && options.useHttp !== null ? options.useHttp : true;
-        this.useHttps = options.useHttps !== undefined && options.useHttps !== null ? options.useHttps : false;
-        this.port = options.port || "8080";
-        this.sslPort = options.sslPort || "8081";
-        this.httpOptions = options.httpOptions || {};
-        this.httpsOptions = options.httpsOptions || {};
-        this.serverHttp = null;
-        this.serverHttps = null;
-        this.routes = [];
-        this.defaultRoute = null;
-    }
-
-    start () {
-        if (this.useHttp) {
-            this.serverHttp = http.createServer(this.handleRequest.bind(this));
-            this.serverHttp.listen(this.port);
-        }
-        if (this.useHttps) {
-            this.serverHttps = https.createServer(this.httpsOptions, this.handleRequest.bind(this));
-            this.serverHttps.listen(this.sslPort);
-        }
-    }
-
-    registerDefaultHandler (handler) {
-        this.defaultRoute = handler;
-    }
-
-    registerHandler (handler) {
-        for (let i = 0; i < this.routes.length; i++) {
-            if (this.routes[i].testRoute(handler.route)) {
-                throw new Error("New route: " + handler.route + " already covered by route: " + this.routes[i].route);
-            }
-        }
-
-        this.routes.push(handler);
-    }
-
-    handleRequest (request, response) {
-        const url = request.url;
-
-        let handled = false;
-        for (let i = 0; i < this.routes.length; i++) {
-            if (this.routes[i].testRoute(url)) {
-                this.routes[i].handleRequest(request, response);
-                handled = true;
-                break;
-            }
-        }
-
-        if (!handled) {
-            this.defaultRoute.handleRequest(request, response);
-        }
-    }
-    
-    getRawHttpServer () {
-        return this.serverHttp;
-    }
-
-    getRawHttpsServer () {
-        return this.serverHttps;
-    }
-
-    stop (callback) {
-        if (this.serverHttp) {
-            this.stopHttp((err) => {
-                if (this.serverHttps) {
-                    this.stopHttps((err2) => {
-                        callback(err || err2);
-                    });
-                } else {
-                    callback(err, null);
-                }
-            });
-        } else if (this.serverHttps) {
-            this.stopHttps(callback);
-        } else {
-            callback();
-        }
-    }
-
-    stopHttp (callback) {
-        this.serverHttp.close((err) => {
-            this.serverHttp = null;
-            if (callback) {
-                callback(err, null);
-            }
-        });
-    }
-
-    stopHttps (callback) {
-        this.serverHttps.close((err) => {
-            this.serverHttps = null;
-            if (callback) {
-                callback(err, null);
-            }
-        });
-    }
-}
-
-const Routes = require("./lib/route"),
-    StaticFileServer = require("./lib/static");
-
-module.exports = BasicHTTPServer;
-module.exports.BaseRoute = Routes.BaseRoute;
-module.exports.NullRoute = Routes.NullRoute;
-module.exports.JSONRoute = Routes.JSONRoute;
-module.exports.StaticFileServer = StaticFileServer;
-
-// const fileHandlerTest = new StaticFileServer("/test", "./", "index.html");
-// const fileHandlerTest2 = new StaticFileServer("/test2", "./", "index.html");
-// const fileHandler = new StaticFileServer("/", "./", "index.html");
-// const httpServer = new BasicHTTPServer({port: 8080});
-// httpServer.registerHandler(fileHandlerTest2);
-// httpServer.registerHandler(fileHandlerTest);
-// httpServer.registerDefaultHandler(fileHandler);
-// httpServer.start();
-// console.log("HTTP Server has started on port: " + httpServer.port);
-//httpServer.stop();
+module.exports = {
+    MicroService: Server,
+    RouteBase: Routes.BaseRoute,
+    RouteNull: Routes.BaseRoute,
+    RouteJSON: Routes.BaseRoute,
+    StaticFileServer: StaticFileServer
+};
